42bc5f453a46da43f23eca993a76e86d6c1823d2
snowbeauti <snowbeauti@naver.com>
2025-04-14 16:44:35 +0900

    수업내용

---
+++ mod4.py
@@ -0,0 +1,7 @@
+import mod1
+#import mymod.mod2 #mymod 패키지를 의미
+#import mymod
+from mymod import mod2 #from 패키지 import 하위
+
+import os 
+print(os.getcwd())
---
+++ mod6.py
@@ -0,0 +1,2 @@
+from game.graphic.render import render_test
+render_test()
